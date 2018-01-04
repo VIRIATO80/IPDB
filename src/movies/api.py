5d2e64bffdcef89b7ed8b12d807d164cbe4d092a
DESKTOP-VSIK1O3\VIRIATO <javi.lindo@gmail.com>
2018-01-04 08:11:42 +0100

    commit

---
+++ src/movies/api.py
@@ -0,0 +1,36 @@
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.viewsets import ModelViewSet
+
+from movies.models import Movie, Category
+from movies.permissions import MoviesPermission, CategoriesPermission
+from movies.serializers import MoviesListSerializer, MovieSerializer, CategorySerializer
+
+
+class MoviesListAPI(ListCreateAPIView):
+
+    queryset = Movie.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_serializer_class(self):
+        return MoviesListSerializer if self.request.method == 'GET' else MovieSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(self.request.user)
+
+
+class MovieDetailAPI(RetrieveUpdateDestroyAPIView):
+
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    permission_classes = [MoviesPermission]
+
+    def perform_update(self, serializer):
+        serializer.save(self.request.user)
+
+
+class CategoryViewSet(ModelViewSet):
+
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [CategoriesPermission]
---
+++ src/movies/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from movies.models import Movie, Category
+from users.serializers import UserSerializer
+
+
+class MoviesListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'image']
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
